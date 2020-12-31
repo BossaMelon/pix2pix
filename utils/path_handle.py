7f3d7e5e878ee889c178e9e54fc389def09534fa
BossaMelon <wyh453255505@gmail.com>
2020-12-31 20:42:01 +0100

    modify resource root

--- utils/path_handle.py
+++ utils/path_handle.py
@@ -4,20 +4,22 @@ from datetime import datetime
 from pathlib import Path
 
 current_username = getpass.getuser()
+project_root = Path(__file__).parent.parent.resolve()
+project_name = project_root.name
 
 if current_username == 'wyh':
-    project_root = Path(__file__).parent.parent.resolve()
-    resource_root = project_root / 'resources'
-    data_path = resource_root / 'data'
-    pretrained_model_path = resource_root / 'pretrained_models'
-    model_checkpoint_path = resource_root / 'model_checkpoints'
+    project_resource_root = project_root / 'resources'
+    data_path = project_resource_root / 'data'
+    pretrained_model_path = project_resource_root / 'pretrained_models'
+    model_checkpoint_path = project_resource_root / 'model_checkpoints'
 
 elif current_username == 'wangyueh':
-    resource_root = Path('/home/wangyueh/projects/GAN/resources')
+    tnt_resource_root = Path('/home/wangyueh/tmp/project_resources')
+    project_resource_root = tnt_resource_root / project_name
     ssd_root = Path('/phys/ssd/wangyueh')
-    data_path = ssd_root / 'GAN/data'
-    pretrained_model_path = ssd_root / 'GAN/pretrained_models'
-    model_checkpoint_path = resource_root / 'model_checkpoints'
+    data_path = ssd_root / project_name / 'data'
+    pretrained_model_path = ssd_root / project_name / 'pretrained_models'
+    model_checkpoint_path = project_resource_root / 'model_checkpoints'
 
 else:
     raise Exception('no valid data path')
@@ -26,7 +28,7 @@ else:
 def _get_result_path():
     now = datetime.now()
     dt_string = now.strftime("%m%d_%H%M%S")
-    result_root = resource_root / 'results' / dt_string
+    result_root = project_resource_root / 'results' / dt_string
     return result_root
 
 
@@ -35,7 +37,7 @@ visualization_path = result_root_path / 'visualization'
 
 
 def create_folder():
-    folders = [visualization_path, data_path, pretrained_model_path, model_checkpoint_path]
+    folders = [project_resource_root, data_path, visualization_path, data_path, pretrained_model_path, model_checkpoint_path]
     for folder in folders:
         if not os.path.exists(folder):
             os.makedirs(folder)
